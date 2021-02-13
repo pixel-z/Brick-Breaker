64dd5955d261e1e09ff331be780171cecd2d1b56
pixel-z <zishankazi01@gmail.com>
2021-02-13 20:02:13 +0530

    Minor changes

--- ball.py
+++ ball.py
@@ -21,7 +21,7 @@ class Ball:
         self._dead = 0      # dead = 1 means ball hit bottom of border
 
     # checks if collision occurs & changes speed accordingly
-    def borderCollision(self, x, y, LIVES, ball_launched):
+    def __borderCollision(self, x, y, LIVES, ball_launched):
         if x < LEFT:
             self._Xspeed *= -1
         if x > RIGHT:
@@ -43,7 +43,7 @@ class Ball:
                 self._offset = random.randint(0,9)
                 ball.append(Ball())
 
-    def paddleCollision(self, x ,y):
+    def __paddleCollision(self, x ,y):
         if y == 42:
             if x>=paddle._x and x<paddle._x+2:
                 self._Xspeed -= 2
@@ -78,8 +78,8 @@ class Ball:
         bg.grid[40][self._x] = ball_fig
 
     def moveBall(self, LIVES, ball_launched):
-        self.borderCollision(self._x + self._Xspeed, self._y - self._Yspeed, LIVES, ball_launched)
-        self.paddleCollision(self._x + self._Xspeed, self._y - self._Yspeed)
+        self.__borderCollision(self._x + self._Xspeed, self._y - self._Yspeed, LIVES, ball_launched)
+        self.__paddleCollision(self._x + self._Xspeed, self._y - self._Yspeed)
         bg.grid[self._y][self._x] = ' '
         if self._dead == 0:
             bg.grid[self._y - self._Yspeed][self._x + self._Xspeed] = ball_fig
